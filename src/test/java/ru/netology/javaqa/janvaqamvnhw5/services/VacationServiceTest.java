87e7bcc4d6cce82a0e8b4ed712cd25667d79817a
Anastasiia Prokhorova <naumtasya@gmail.com>
2023-09-10 00:01:00 +0700

    Csv File Source

--- src/test/java/ru/netology/javaqa/janvaqamvnhw5/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/janvaqamvnhw5/services/VacationServiceTest.java
@@ -1,31 +1,37 @@
 package ru.netology.javaqa.janvaqamvnhw5.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void shouldCalculateFormMonthsVacations() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files="src/test/resources/monthsVacation.csv")
+    public void shouldCalculateFormMonthsVacations(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
 
-        int expected = 3;
+        //int expected = 3;
         // вызываем целевой метод:
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldCalculateFormMonthsVacations100k() {
-        VacationService service = new VacationService();
-
-        int expected = 2;
-        // вызываем целевой метод:
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
+//  @Test
+//    public void shouldCalculateFormMonthsVacations100k() {
+//        VacationService service = new VacationService();
+//
+//        int expected = 2;
+//        // вызываем целевой метод:
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//
+//        // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
